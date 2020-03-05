a9827eb1a3fe4905a75141def9b5e33f5668cbc7
unknown <ig231996@gmail.com>
2020-03-05 20:09:51 +0100

    functional map done

--- app.R
+++ app.R
@@ -8,42 +8,79 @@
 #
 
 library(shiny)
+library(leaflet)
+library(RColorBrewer)
 source('utils/utils.R')
 
 data = data_downloader()
+
+r_colors <- rgb(t(col2rgb(colors()) / 255))
+names(r_colors) <- colors()
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+ui <- bootstrapPage(
+    tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
+    leafletOutput("map", width = "100%", height = "100%"),
+    absolutePanel(top = 10, right = 10,
+                  sliderInput("range", "Mean Income", min(data$Mean), max(data$Mean),
+                              value = range(data$Mean), step = 0.1
+                  ),
+                  selectInput("colors", "Color Scheme",
+                              rownames(subset(brewer.pal.info, category %in% c("seq", "div")))
+                  ),
+                  checkboxInput("legend", "Show legend", TRUE)
     )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- data$Mean
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    # Reactive expression for the data subsetted to what the user selected
+    filteredData <- reactive({
+        data[data$Mean >= input$range[1] & data$Mean <= input$range[2],]
+    })
+    
+    # This reactive expression represents the palette function,
+    # which changes as the user makes selections in UI.
+    colorpal <- reactive({
+        colorNumeric(input$colors, data$Mean)
+    })
+    
+    output$map <- renderLeaflet({
+        # Use leaflet() here, and only include aspects of the map that
+        # won't need to change dynamically (at least, not unless the
+        # entire map is being torn down and recreated)
+        leaflet(data) %>% addTiles() %>%
+            fitBounds(~min(Lon), ~min(Lat), ~max(Lon), ~max(Lat))
+    
+    })
+    
+    # Incremental changes to the map (in this case, replacing the
+    # circles when a new color is chosen) should be performed in
+    # an observer. Each independent set of things that can change
+    # should be managed in its own observer.
+    observe({
+        pal <- colorpal()
+        
+        leafletProxy("map", data = filteredData()) %>%
+            clearShapes() %>%
+            addCircles(radius = ~Mean*2/100, weight = 1, color = "#777777",
+                       fillColor = ~pal(Mean), fillOpacity = 0.7, popup = ~paste(Mean)
+            )
+    })
+    
+    # Use a separate observer to recreate the legend as needed.
+    observe({
+        proxy <- leafletProxy("map", data = data)
+        
+        # Remove any existing legend, and only if the legend is
+        # enabled, create a new one.
+        proxy %>% clearControls()
+        if (input$legend) {
+            pal <- colorpal()
+            proxy %>% addLegend(position = "bottomright",
+                                pal = pal, values = ~Mean
+            )
+        }
     })
 }
 
