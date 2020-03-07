a674b3e0338ca79cfdb89ef7b142c8eaeb084bfe
unknown <ig231996@gmail.com>
2020-03-07 18:59:08 +0100

    Bootstrap map

--- app.R
+++ app.R
@@ -41,20 +41,6 @@ dataSelection = absolutePanel(top = 10, right = 10,
                                downloadButton('downloadData', 'Download selected data')
                 )
 
-# headerRow = div(id='header', useShinyjs(),
-#                 absolutePanel(
-#                               sliderInput("range", "Mean Income", min(data$Mean), max(data$Mean),
-#                                           value = range(data$Mean), step = 0.1
-#                               ),
-#                               selectInput("selState", 
-#                                           label="Select State", 
-#                                           multiple = TRUE,
-#                                           choices = state_names),
-#                               selectInput("colors", "Color Scheme",
-#                                           rownames(subset(brewer.pal.info, category %in% c("seq", "div")))
-#                               ),
-#                               checkboxInput("legend", "Show legend", TRUE)
-# ))
 
 
 map = leafletOutput("map", width = "100%", height = "90vh")
@@ -74,8 +60,34 @@ dataPanel <- tabPanel("Data",
 
 
 # ui = navbarPage('US Income', id = 'navBar',header = headerRow, mapPanel, dataPanel)
-ui = navbarPage('US Income', id = 'navBar', mapPanel, dataPanel)
+#ui = navbarPage('US Income', id = 'navBar', mapPanel, dataPanel)
 
+ui <- bootstrapPage(
+
+                  tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
+                  
+                  leafletOutput("map", width = "100%", height = "100%"),
+                  
+                  absolutePanel(top = 10, right = 10,
+                                                sliderInput("range", "Mean Income", min(data$Mean), max(data$Mean),
+                                                            value = range(data$Mean), step = 0.1
+                                                ),
+                                                selectInput("selState", 
+                                                            label="Select State", 
+                                                            multiple = TRUE,
+                                                            choices = state_names),
+                                                selectInput("selCounty", 
+                                                            label="Select County", 
+                                                            multiple = TRUE,
+                                                            choices = county_names),
+                                                selectInput("colors", "Color Scheme",
+                                                            rownames(subset(brewer.pal.info, category %in% c("seq", "div")))
+                                                ),
+                                                checkboxInput("legend", "Show legend", TRUE),
+                                                downloadButton('downloadData', 'Download selected data')
+                  )
+                  
+    )
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
