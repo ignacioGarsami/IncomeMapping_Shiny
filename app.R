9ad59e2c1b12307dbc8e47690139e17361cce6f3
unknown <ig231996@gmail.com>
2020-03-10 13:50:53 +0100

    added data tab with plots

--- app.R
+++ app.R
@@ -58,6 +58,22 @@ dataPanel <- tabPanel("Data",
                       
 )
 
+dataPanel <- tabPanel("Data",
+                      fluidRow(
+                          column(width = 6,height = 20,
+                                plotly::plotlyOutput('plotly_income')),
+                          column(width = 6, height = 20,
+                                 plotly::plotlyOutput("plotly_bar")
+                          )
+                      ),
+                      div(id="Data_table",
+                          tableOutput('dataTable')
+                      )
+)
+
+
+
+
 
 ui = navbarPage('US Income', id = 'navBar', mapPanel, dataPanel)
 
@@ -153,6 +169,21 @@ server <- function(input, output, session) {
         },
         content = function(con) {
             write.csv(filteredData(), con)
+        }
+    )
+    
+    output$plotly_bar <- plotly::renderPlotly(
+        if(is.null(input$selState) == FALSE & is.null(input$selCounty) == FALSE){
+            ggplot(filteredData(), aes(x=County, y=Mean, fill=State_Name))
+            + geom_bar(stat="identity", position=position_dodge()) + theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+                labs(ylab = 'Mean income', fill = 'State name')
+        }
+    )
+    output$plotly_income <- plotly::renderPlotly(
+        if(is.null(input$selState) == FALSE & is.null(input$selCounty) == FALSE){
+            ggplot(filteredData()) + aes(x=County, y=Mean, fill=State_Name)
+            + geom_boxplot() + theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+                labs(ylab = 'Mean income', fill = 'State name')
         }
     )
 }
